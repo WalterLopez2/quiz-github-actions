9f6ac450cd00ca7b2cbd717a208e9192e1f7f765
WalterLopez2 <javlopez235@gmail.com>
2025-12-06 16:55:48 -0600

    Ejercicio 2

---
+++ Ejercicio 2/calculadora.js
@@ -0,0 +1,7 @@
+function sumar(a, b) { return a + b; } 
+ 
+function restar(a, b) { return a - b; } 
+ 
+function multiplicar(a, b) { return a * b; } 
+ 
+module.exports = { sumar, restar, multiplicar }; 
---
+++ Ejercicio 2/test-calculadora.js
@@ -0,0 +1,30 @@
+const { sumar, restar, multiplicar } = require('./calculadora.js'); 
+let pasados = 0; 
+let fallados = 0; 
+ 
+if (sumar(2, 3) === 5) { 
+    console.log("Test 1 pasó: suma(2, 3) = 5"); 
+    pasados++; 
+} else { 
+    console.log("Test 1 falló"); 
+    fallados++; 
+} 
+ 
+if (restar(10, 5) === 5) { 
+    console.log("Test 2 pasó: resta(10, 5) = 5"); 
+    pasados++; 
+} else { 
+    console.log("Test 2 falló"); 
+    fallados++; 
+} 
+ 
+if (multiplicar(4, 5) === 20) { 
+    console.log("Test 3 pasó: multiplicar(4, 5) = 20"); 
+    pasados++; 
+} else { 
+    console.log("Test 3 falló"); 
+    fallados++; 
+} 
+ 
+console.log("\nTotal: " + pasados + " pasados, " + fallados + " fallados"); 
+if (fallados > 0) { process.exit(1); }
